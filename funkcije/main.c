5d30454f8d8d4bfd57e030a6e3c2c2846ac45969
AleksandarV <a.vujic@live.com>
2016-10-11 20:06:32 +0200

    Ovo je kod kojim se daje primer kako funkcije funkcionisu

--- funkcije/main.c
+++ funkcije/main.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 
+//prototipovi funkcija
+void f1(int);
+void f2(int *);
+
+//glavna funkcija
 int main(void)
 {
-    printf("Hello World!\n");
-    return 0;
-    //komentar
-    //komentar 2
+int x=5;
+
+printf("Na pocetku je funkcija %d\n",x);
+
+f1(x);
+
+printf("Vrednost x-a nakon prve funkcije %d\n",x);
+
+
+f2(&x);
+
+printf("Vrednost x-a nakon druge funkcije %d\n",x);
+printf("Adresa x-a(trebalo bi da bude ista sa a): %d",&x);
+
+return 0;
+}
+//definicije funkcija
+void f1(int a){
+    a=3;
+}
+
+void f2(int *a){
+    *a=3;
+    printf("Adresa pointer-a je %d\n", &a);
+    printf("Sadrzaj pointera(ustvari adresa trazene promenljive %d\n", a);
 }
 
